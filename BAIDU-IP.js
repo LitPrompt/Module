ac84edb1f4d12d2cb877878cf9c873e8130c76b7
QGCliveDavis <mc2267599808@hotmail.com>
2022-06-30 10:15:55 +0800

    Update BAIDU-IP.js

--- BAIDU-IP.js
+++ BAIDU-IP.js
@@ -1,14 +1,12 @@
-var ip=data
-$httpClient.get("https://api.my-ip.io", function (error, response, data) {})
-$httpClient.get(`http://ip-api.com/json/${ip}`, function (error, response, data1) {
 
-    let jsonData1 = JSON.parse(data1)
-    let city2 = jsonData1.city
-    let ip2 = jsonData1.query
+$httpClient.get("http://ip-api.com/json/", function (error, response, data) {
+    let jsonData1 = JSON.parse(data)
+    let city1 = jsonData1.city
+    let ip1 = jsonData1.query
 
     body={
         title: "当前免流信息",
-        content: `免流IP：${ip2}\n地区：${city2}`,
+        content: `免流IP：${ip1}\n地区：${city1}`,
         backgroundColor: "#663399",
         icon: "network",
     }

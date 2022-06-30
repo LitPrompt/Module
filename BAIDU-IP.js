aba406c3f56a78c69488fef17ef37ee99a0d7f73
QGCliveDavis <mc2267599808@hotmail.com>
2022-06-30 08:09:04 +0800

    Update BAIDU-IP.js

--- BAIDU-IP.js
+++ BAIDU-IP.js
@@ -1,7 +1,9 @@
 $httpClient.get("http://ip-api.com/json", function (error, response, data) {
+    let isp = jsonData.isp
+    let ip = jsonData.query
     $done({
         title: "当前免流信息",
-        content: `IP信息：${jsonData.query}\n运营商：${jsonData.isp}\n`,
+        content: `IP信息：${ip}\n运营商：${isp}\n`,
         backgroundColor: "#663399",
         icon: "network",
     })

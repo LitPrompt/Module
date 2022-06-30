76043fa0b13a3d17d918401d7cc34985d2be1715
QGCliveDavis <mc2267599808@hotmail.com>
2022-06-30 11:15:00 +0800

    Update BAIDU-IP.js

--- STASH/BAIDU-IP.js
+++ STASH/BAIDU-IP.js
@@ -6,7 +6,7 @@ $httpClient.get("http://ip-api.com/json", function (error, response, data) {
     body={
         title: "当前免流信息",
         content: `免流IP：${ip}\n地区：${country} - ${city}`,
-        backgroundColor: "#0C9DFA",
+        backgroundColor: "#268F81",
         icon: "globe.asia.australia.fill",
     }
     $done(body)

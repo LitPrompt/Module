4e094059440f001bdcc481f1ef48c62c7220957d
QGCliveDavis <mc2267599808@hotmail.com>
2022-07-01 22:20:53 +0800

    Update BAIDU-IP.js

--- STASH/BAIDU-IP.js
+++ STASH/BAIDU-IP.js
@@ -1,7 +1,7 @@
 $httpClient.get("https://forge.speedtest.cn/api/location/info", function (error, response, data) {
-    const dataObject = JSON.parse(data);
+    let dataObject = JSON.parse(data);
     let { country, province, city, ip } = dataObject;
-    const region = `地区：${country} ${province} ${city}`;
+    let region = `地区：${country} ${province} ${city}`;
     ip = `免流IP：${ip}`;
     body={
         title: "当前免流信息",

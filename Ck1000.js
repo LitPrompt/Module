2fc2fbd5edc1b009684d529bbac72829b44a2a7a
Cat <coutngg123@gmail.com>
2022-08-11 16:40:00 +0800

    cat

---
+++ Ck1000.js
@@ -0,0 +1,15 @@
+const cookieName = '中国电信'
+const cookieKey = 'Tele_CK'
+const cookieVal = $request.headers['Cookie']
+
+if (cookieVal) {
+  let cookie = $persistentStore.write(cookieVal, Tele_CK)
+    if (cookie) {
+        let msg = `${cookieName}`
+            $notification.post(msg, 'Cookie写入成功', )
+            console.log(msg)
+            console.log(cookieVal)
+        }
+    }
+
+$done({})

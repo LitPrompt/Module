d5ec38728e74298098d1d8d4c25dd0aa241eab70
Cat <other>
2022-08-12 02:12:37 +0800

    cat

--- TeleV1.0/10000.js
+++ TeleV1.0/10000.js
@@ -107,7 +107,7 @@ $httpClient.post(
 function notice()
 {
 
-	for(var s=0;jsonData.items[s].offerType==11;s++)//从json中筛选出卡名
+	for(var s=0;jsonData.items[s].offerType!=11;s++)//从json中筛选出卡名
 	brond = jsonData.items[s].productOFFName
 	limitbalancetotal=(limitbalancetotal/1048576).toFixed(2) //剩余转成mb保留两位
   unlimitusagetotal=(unlimitusagetotal/1048576).toFixed(2)//总免使用转化成gb保留两位小数

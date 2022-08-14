c831d543a6d38f2ebc48536b932f9071af59a4f5
QGCliveDavis <mc2267599808@hotmail.com>
2022-08-14 09:52:02 +0800

    Update 10000.js

--- TeleV1.0/10000.js
+++ TeleV1.0/10000.js
@@ -82,6 +82,10 @@ $httpClient.post(
   	unlimitThis=unlimitusagetotal //将当前查询的值存到unlimitThis中
 	limitLast=$persistentStore.read("limitStore") //将上次查询到的值读出来
   	unlimitLast=$persistentStore.read("unlimitStore") //将上次查询到的值读出来
+	console.log("当前通用使用"+limitThis)
+	console.log("当前定向使用"+unlimitThis)
+	console.log("上次通用使用"+unlimitLast)
+	console.log("上次定向使用"+unlimitLast)
 	if(limitLast==null||limitThis-limitLast<0)
 	{
 		limitLast=0
@@ -121,14 +125,14 @@ function notice()
 	brond=$persistentStore.read("key_brond")
 	if(typeof brond=="undefined")
 	{
-	for(var s=0;s+1<=i;s++)
-		{
-			var typeid = jsonData.items[s].offerType
-			if(typeid==11){var brondid=s}
+		for(var s=0;s+1<=i;s++)
+			{
+				var typeid = jsonData.items[s].offerType
+				if(typeid==11){var brondid=s}
+			}
+			brond = jsonData.items[brondid].productOFFName
+			$persistentStore.write(brond,"key_brond")
 		}
-		brond = jsonData.items[brondid].productOFFName
-		$persistentStore.write(brond,"key_brond")
-	}
 	limitUsed=(limitChange/1024).toFixed(3) //跳点转成mb保留三位
 	unlimitUsed=(unlimitChange/1024).toFixed(2) //免流转成mb保留两位
 	limitbalancetotal=(limitbalancetotal/1048576).toFixed(2) //剩余转成mb保留两位
@@ -140,8 +144,9 @@ function notice()
 			$persistentStore.write(thishours,"hourstimeStore")
 			$persistentStore.write(thisminutes,"minutestimeStore") 
 			$notification.post(brond+'  耗时:'+minutesused+'分钟','免'+unlimitUsed+' MB '+' 跳'+limitUsed+' MB','总免'+unlimitusagetotal+' GB '+' 剩余'+limitbalancetotal+' GB')
-		  //console.log(brond+'  耗时:'+minutesused+'分钟'+'  总免 '+unlimitusagetotal+' GB')
-		  //console.log('免'+unlimitUsed+' MB '+' 跳 '+limitUsed+'MB'+'剩余 '+limitbalancetotal+'GB')
+		  	console.log(brond+'  耗时:'+minutesused+'分钟')
+		  	console.log('免'+unlimitUsed+' MB '+' 跳'+limitUsed+' MB')
+			console.log('总免'+unlimitusagetotal+' GB '+' 剩余'+limitbalancetotal+' GB')
 		}
 	}
 	else//默认定时通知
@@ -149,7 +154,10 @@ function notice()
 		$persistentStore.write(thisminutes,"minutestimeStore")  
 		$persistentStore.write(thishours,"hourstimeStore")
 		$notification.post(brond+'  耗时:'+minutesused+'分钟','免'+unlimitUsed+' MB '+' 跳'+limitUsed+' MB','总免'+unlimitusagetotal+' GB '+' 剩余'+limitbalancetotal+' GB')	
-		//console.log('免'+unlimitUsed+' MB '+' 跳'+limitUsed+' MB '+' 剩余'+limitbalancetotal+' GB')
+		console.log(brond+'  耗时:'+minutesused+'分钟')
+		console.log('免'+unlimitUsed+' MB '+' 跳'+limitUsed+' MB')
+		console.log('总免'+unlimitusagetotal+' GB '+' 剩余'+limitbalancetotal+' GB')
+		
 		
 	}
 }

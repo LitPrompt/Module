c309fbc96d6daa9947b526ab0d0ccb8f72b0c9a6
QGCliveDavis <mc2267599808@hotmail.com>
2022-08-16 15:05:38 +0800

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -1,5 +1,5 @@
 if ($response.body) {
-    $notification.post("识别成功","识别成功",)
+    $notification.post("识别成功","识别成功",$response.body)
     $done({
         body: JSON.stringify({
             "product_infos": [{

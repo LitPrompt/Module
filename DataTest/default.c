c1d73dd69c8413a2cebda0274748910e53ad56c1
Cat <coutngg123@gmail.com>
2022-08-21 21:21:01 +0800

    1

---
+++ DataTest/default.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct LNode{
+	int data;
+   struct LNode* next;
+}LNode,*Linklist;
+
+Linklist headinsert(Linklist &L){
+   int c;
+   LNode *S;
+   Linklist L;
+   L=(Linklist)malloc(sizeof(LNode));
+   L->next=NULL;
+   scanf("%d",&c);
+   while(c!=999){
+      S=(LNode *)malloc(sizeof(LNode));
+      S->data=c;
+      S->next=L->next;
+      L->next=S;
+      scanf("%d",&c);
+   }
+   return L;
+}
+
+//Linklist TailInsert(){}
+
+void printList(Linklist L){
+    while(L != NULL){  
+        printf("%d ", L->data);
+        L = L->next; //向后遍历
+    } 
+}
+
+int main() {
+   Linklist L;
+   headinsert(L);
+   printList(L->next);
+}

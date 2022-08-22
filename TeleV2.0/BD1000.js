d39a5943627e9041a239060dba059fc0cb9823f4
QGCliveDavis <mc2267599808@hotmail.com>
2022-08-22 19:49:15 +0800

    Update BD1000.js

--- TeleV2.0/BD1000.js
+++ TeleV2.0/BD1000.js
@@ -3,8 +3,8 @@ const bodyVal = $request.body
 const effective = bodyVal.indexOf("SESSIONKEY")
 
 if (effective=="0") {
-  let Tele_body = $persistentStore.write(bodyVal, "Tele_BD")
-    if (Tele_body) {
+    $persistentStore.write(bodyVal, "Tele_BD")
+    if (bodyVal) {
         let msg = `${bodyName}`
             $notification.post(msg, 'Body写入成功',bodyVal )
             console.log(msg)

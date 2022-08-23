c92220aa1c185617e4df57e966104fb41232ea30
QGCliveDavis <mc2267599808@hotmail.com>
2022-08-23 13:15:27 +0800

    Update 10000.js

--- TeleV2.0/10000.js
+++ TeleV2.0/10000.js
@@ -1,3 +1,5 @@
+const $ = new Env(`电信余量`)
+
 var ns = $persistentStore.read("notice_switch");
 var auto = $persistentStore.read("auto_switch");
 var Tele_body = $persistentStore.read("Tele_BD");
@@ -44,6 +46,7 @@ $httpClient.post(
   },
   (error, response, data) => {
   
+	$.notice("测试","test","test")
     // console.log(data)
   	jsonData = JSON.parse(data)
 	var logininfo=jsonData.RESPONSECODE
@@ -254,4 +257,37 @@ function cellular_choose()
 	console.log(limitusagetotal)
 	console.log("")
 }
-	
+
+function Env(name) {
+	LN = typeof $loon != `undefined`
+	SG = typeof $httpClient != `undefined` && !LN
+	QX = typeof $task != `undefined`
+	SH = typeof $environment && $environment['stash-version']!== `undefined`
+	read = (key) => {
+	  if (LN || SG || SH) return $persistentStore.read(key)
+	  if (QX) return $prefs.valueForKey(key)
+	}
+	write = (key, val) => {
+	  if (LN || SG || SH) return $persistentStore.write(key, val); 
+	  if (QX) return $prefs.setValueForKey(key, val)
+	}
+	notice = (title, subtitle, message, url) => {
+	  if (LN) $notification.post(title, subtitle, message, url)
+	  if (SG || SH) $notification.post(title, subtitle, message, { url: url })
+	  if (QX) $notify(title, subtitle, message, { 'open-url': url })
+
+	}
+	get = (url, cb) => {
+	  if (LN || SG || SH) {$httpClient.get(url, cb)}
+	  if (QX) {url.method = `GET`; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
+	}
+	post = (url, cb) => {
+	  if (LN || SG || SH) {$httpClient.post(url, cb)}
+	  if (QX) {url.method = `POST`; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
+	}
+	toObj = (str) => JSON.parse(str)
+	toStr = (obj) => JSON.stringify(obj)
+	log = (message) => console.log(message)
+	done = (value = {}) => {$done(value)}
+	return { name, read, write, notice, get, post, toObj, toStr, log, done }
+  }

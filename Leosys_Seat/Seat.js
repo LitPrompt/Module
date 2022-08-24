618e550b3d0dd45fe379e94858661ad572965269
QGCliveDavis <mc2267599808@hotmail.com>
2022-08-24 12:29:24 +0800

    1

--- Leosys_Seat/Seat.js
+++ Leosys_Seat/Seat.js
@@ -1,9 +1,10 @@
-const url = `https://leosys.cn/axhu/rest/v2/room/layoutByDate/2/2022-08-24`;
+let url = `https://leosys.cn/axhu/rest/v2/room/layoutByDate/2/2022-08-24`;
 const headers = {
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.26(0x18001a31) NetType/WIFI Language/zh_CN`,
 'token' : $persistentStore.read("LeoSys_Token"),
 };
 const body = ``;
+let seat=$persistentStore.read("tsg_seat")
 
 $httpClient.get(
     {
@@ -12,9 +13,13 @@ $httpClient.get(
       body: body, 
     },
     (error, response, data) => {
-        console.log(data)
-        console.log(typeof data)
         var jsondata = JSON.parse(data);
-        console.log(jsondata.status)
-        $notification.post(jsondata.status,"","")
+        let y=0
+        for(let key in jsondata.data.layout){
+            key=Number(key)
+            if(jsondata.data.layout[key].status=="FREE")
+            {y++}
+        }
+        $notification.post("当前空余座位："+y+"个，快抢!!!")
+
     })

21004475c8df06869f213f8e8164411749bb1278
QGCliveDavis <mc2267599808@hotmail.com>
2022-08-24 10:57:13 +0800

    Update Seat.js

--- Leosys_Seat/Seat.js
+++ Leosys_Seat/Seat.js
@@ -1,7 +1,7 @@
 const url = `https://leosys.cn/axhu/rest/v2/room/layoutByDate/2/2022-08-24`;
 const headers = {
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.26(0x18001a31) NetType/WIFI Language/zh_CN`,
-'token' : $persistentstore.read("LeoSys_Token"),
+'token' : $persistentStore.read("LeoSys_Token"),
 };
 const body = ``;
 

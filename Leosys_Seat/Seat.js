d4e3b1295b3a6b06c9a3627ff98a9798d864568c
QGCliveDavis <mc2267599808@hotmail.com>
2022-08-25 20:44:26 +0800

    Update Seat.js

--- Leosys_Seat/Seat.js
+++ Leosys_Seat/Seat.js
@@ -1,46 +1,66 @@
-let url = `https://leosys.cn/axhu/rest/v2/room/layoutByDate/2/2022-08-24`;
+let tsgseat=Number($persistentStore.read("tsg_seat"))
+// .split(' ')
+
+dateObj = $script.startTime//获取时间
+data=dateObj.getDate()+1
+month=dateObj.getMonth()+1
+year=dateObj.getFullYear()
+let time=year+"-"+month+"-"+data
+
 const headers = {
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.26(0x18001a31) NetType/WIFI Language/zh_CN`,
 'token' : $persistentStore.read("LeoSys_Token"),
 };
 const body = ``;
-let seat=Number($persistentStore.read("tsg_seat"))
-console.log(typeof seat)
-console.log(seat)
-// .split(' ')
 
-function seat_left(){
+$httpClient.get(
+    {
+      url: `https://leosys.cn/axhu/rest/v2/room/layoutByDate/2/${time}`,
+      headers: headers,
+      body: body, 
+    },
+  (error, response, data) => {
+    //console.log(data)
+    jsondata = JSON.parse(data);
+    if(jsondata.status=="fail"){
+        $httpClient.get(
+            {
+                url: 'https://leosys.cn/axhu/rest/auth?username=1942065227&password=NAZHVX.H.1VH%5BYBF',
+                headers: {
+                    'token' : $persistentStore.read("LeoSys_Token"),
+                    'actCode' : `true`},
+                body: body, 
+            },(error,response,data)=>{
+                tokendata = JSON.parse(data);
+                $persistentStore.write(tokendata.data.token,"LeoSys_Token")})}
+    else{
+        onsole.log("当前空余座位："+total_seat(jsondata)+"个  "+"座位状态："+get_seat(tsgseat,jsondata))
+
+        $notification.post('当前空余座位：'+total_seat(jsondata)+'个','座位状态：'+get_seat(tsgseat,jsondata),'')
+    }
+    $done()
+})
+
+
+function total_seat(jsondata){
     let y=0
-    for(var k in jsondata.data.layout){
-        if(jsondata.data.layout[Number(k)].status=="FREE")
+    for(var k in jsondata.data.layout)
+    {
+        const datasys=jsondata.data.layout[Number(k)]
+        if(datasys.status=="FREE"&&datasys.type=="seat")
         {y++}
     }
     return y
 }
 
-function seat_get(a){
+function get_seat(seat,jsondata){
     for(var k in jsondata.data.layout){
-        if(jsondata.data.layout[Number(k)].name==a){
-            if(jsondata.data.layout[Number(k)].status=="FREE"){return '所选座位没有人'}
-            else{return '所选座位有人'}
-        }
+    if(jsondata.data.layout[Number(k)].name==seat)
+    {
+        if(jsondata.data.layout[Number(k)].status=="FREE")
+        {return '所选座位没有人'}
+        else
+        {return '所选座位有人'}
+    }
     }
 }
-
-$httpClient.get(
-    {
-      url: url,
-      headers: headers,
-      body: body, 
-    },
-    (error, response, data) => {
-        var jsondata = JSON.parse(data);
-        if(jsondata.status=="fail"){$notification.post("Token已过期，请重新抓取Token",jsondata.message,"")}
-        
-        console.log("当前空余座位："+seat_left()+"个，快抢!!!")
-        console.log("所选座位状态："+seat_get(seat))
-        $notification.post('当前空余座位：'+seat_left()+'个','座位状态：'+seat_get(seat),'')
-
-        $done()
-    })
- 

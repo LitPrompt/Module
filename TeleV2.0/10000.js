6d4c3859118b130a52c2cb60cdeb3028db422e65
QGCliveDavis <mc2267599808@hotmail.com>
2022-08-27 20:36:48 +0800

    Update 10000.js

--- TeleV2.0/10000.js
+++ TeleV2.0/10000.js
@@ -106,7 +106,13 @@ $httpClient.post(
 //*******
 
 	notice()//通知部分
-	//tiles()
+
+	body={
+        title: "当前免流信息",
+        content: `TEST`,
+        backgroundColor: "#FF6600",
+        icon: "antenna.radiowaves.left.and.right.circle.fill",
+    }
 	$done()
   }
  

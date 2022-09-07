f279623880037fd5e3de5abce2065f1016223a79
Cat <other>
2022-09-07 17:26:48 +0800

    1

---
+++ Env for me/env.js
@@ -0,0 +1,33 @@
+const $ = new Env(`Env for me`)
+
+function Env(name) {
+  LN = typeof $loon != `undefined`
+  SG/STASH = typeof $httpClient != `undefined` && !LN
+  QX = typeof $task != `undefined`
+  read = (key) => {
+    if (LN || SG/STASH) return $persistentStore.read(key)
+    if (QX) return $prefs.valueForKey(key)
+  }
+  write = (key, val) => {
+    if (LN || SG/STASH) return $persistentStore.write(key, val); 
+    if (QX) return $prefs.setValueForKey(key, val)
+  }
+  notice = (title, subtitle, message, url) => {
+    if (LN) $notification.post(title, subtitle, message, url)
+    if (SG/STASH) $notification.post(title, subtitle, message, { url: url })
+    if (QX) $notify(title, subtitle, message, { 'open-url': url })
+  }
+  get = (url, cb) => {
+    if (LN || SG/STASH) {$httpClient.get(url, cb)}
+    if (QX) {url.method = `GET`; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
+  }
+  post = (url, cb) => {
+    if (LN || SG/STASH) {$httpClient.post(url, cb)}
+    if (QX) {url.method = `POST`; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
+  }
+  toObj = (str) => JSON.parse(str)
+  toStr = (obj) => JSON.stringify(obj)
+  log = (message) => console.log(message)
+  done = (value = {}) => {$done(value)}
+  return { name, read, write, notice, get, post, toObj, toStr, log, done }
+}

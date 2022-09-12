04584739a4482e5482d17644cf7dfb6e80d7c0ca
Cat <countgg123@gmail.com>
2022-09-12 16:10:14 +0800

    1

--- Telecom/GetBd.js
+++ Telecom/GetBd.js
@@ -9,6 +9,7 @@ czapp.bestpay.com.cn
 
 const $ = new Env('中国电信获取body');
 const effective = $request.body.indexOf("BILLCYCLE")
+let loginerror=$.read('Bodyswitch')
 
 !(async () => {await GetBody();})().catch((e) => {$.log(e)}).finally(() => {$.done({});});
 

9277f393eac7cdda339e9cf04271ab73e6d3b1b4
QGCliveDavis <72721151+QGCliveDavis@users.noreply.github.com>
2022-11-27 13:19:26 +0800

    更新 Tele_Cellular_Auto.js

--- Telecom/Tele_Cellular_Auto.js
+++ Telecom/Tele_Cellular_Auto.js
@@ -140,8 +140,7 @@ const Tele_AutoCheck_unlimittoday=`Tele_AutoCheck.unlimittoday`
             notice_body = $.getdata(Tele_AutoCheck_notice_body).split('/')
         } else { notice_body = $.getdata(Tele_AutoCheck_notice_body).split('/') }
         
-        $.log('详细信息：'+$.toStr(AllInfo(jsonData).Phone.Bar)+`\n\n`+ '流量卡名：' + brond + `\n`+'账户余额：'+AllInfo(jsonData).Phone.Left+` `+'实时话费：'+AllInfo(jsonData).Phone.Used+`\n`+AllInfo(jsonData).Flow.Detail+`\n`+'国内语音/'+AllInfo(jsonData).Voice.Total+' 使用'+AllInfo(jsonData).Voice.Used+` 剩余`+AllInfo(jsonData).Voice.Used+`\n`+AllInfo(jsonData).Storage.Detail+`\n`+'流量总共使用：'+AllInfo(jsonData).Flow.AllUsed+`\n`+'云盘总共使用：'+AllInfo(jsonData).Storage.AllUsed+`\n`+AllInfo(jsonData).Integral+`\n`)
-
+        $.log('详细信息：'+$.toStr(AllInfo(jsonData).Phone.Bar)+`\n\n`+ '流量卡名：' + brond + `\n`+'账户余额：'+AllInfo(jsonData).Phone.Left+` `+'实时话费：'+AllInfo(jsonData).Phone.Used+`\n`+AllInfo(jsonData).Flow.Detail+`\n`+'国内语音/'+AllInfo(jsonData).Voice.Total+' 使用：'+AllInfo(jsonData).Voice.Used+` 剩余：`+AllInfo(jsonData).Voice.Used+`\n`+AllInfo(jsonData).Storage.Detail+`\n`+'流量总共使用：'+AllInfo(jsonData).Flow.AllUsed+`\n`+'云盘总共使用：'+AllInfo(jsonData).Storage.AllUsed+`\n`+AllInfo(jsonData).Integral+`\n`)
         $.log("上次通用使用：" + ToSize(limitLast, 2, 0, 1) + " 当前通用使用：" + ToSize(limitThis, 2, 0, 1))
         $.log("上次定向使用：" + ToSize(unlimitLast, 2, 0, 1) + " 当前定向使用：" + ToSize(unlimitThis, 2, 0, 1))
         $.log("通用变化量：" + ToSize(limitChange, 2, 0, 1) + " 定向变化量：" + ToSize(unlimitChange, 2, 0, 1))
@@ -327,11 +326,11 @@ function AllInfo(jsondata){
 	}
 	let FlowInfo={
 		Detail:All.flowInfo.flowList[0].title+All.flowInfo.flowList[0].rightTitleEnd
-		+' '+All.flowInfo.flowList[0].leftTitle+All.flowInfo.flowList[0].leftTitleHh
-		+' '+All.flowInfo.flowList[0].rightTitle+All.flowInfo.flowList[0].rightTitleHh
+		+' '+All.flowInfo.flowList[0].leftTitle+'：'+All.flowInfo.flowList[0].leftTitleHh
+		+' '+All.flowInfo.flowList[0].rightTitle+'：'+All.flowInfo.flowList[0].rightTitleHh
 		+`\n`+All.flowInfo.flowList[1].title+All.flowInfo.flowList[1].rightTitleEnd
-		+` `+All.flowInfo.flowList[1].leftTitle+All.flowInfo.flowList[1].leftTitleHh
-		+` `+All.flowInfo.flowList[1].rightTitle+All.flowInfo.flowList[1].rightTitleHh,
+		+` `+All.flowInfo.flowList[1].leftTitle+'：'+All.flowInfo.flowList[1].leftTitleHh
+		+` `+All.flowInfo.flowList[1].rightTitle+'：'+All.flowInfo.flowList[1].rightTitleHh,
 		AllUsed:All.flowInfo.flowRegion.subTitleHh
 	}
 	let VoiceInfo={
